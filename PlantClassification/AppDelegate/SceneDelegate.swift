fe7835c545af2d768097334b9830fe2d73f7e371
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-02-19 21:20:49 +0300

    App Delegate and SceneDelegate added.

---
+++ PlantClassification/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  PlantClassification
+//
+//  Created by Şehriban Yıldırım on 19.02.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let viewController = ViewController()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible() 
+    }
+}
