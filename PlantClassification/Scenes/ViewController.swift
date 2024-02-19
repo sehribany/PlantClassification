8a4138ba87f29ec0077bd6d36292ed1040192a14
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-02-19 21:20:58 +0300

    Scenes created.

---
+++ PlantClassification/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantClassification
+//
+//  Created by Şehriban Yıldırım on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
