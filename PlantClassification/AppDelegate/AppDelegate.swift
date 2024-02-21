60f8ef3a6c231ee6e65f0de5159e276394f56490
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-02-21 22:41:11 +0300

    updated project

--- PlantClassification/AppDelegate/AppDelegate.swift
+++ PlantClassification/AppDelegate/AppDelegate.swift
@@ -9,12 +9,10 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        Thread.sleep(forTimeInterval: 3)
+        return false
     }
 
     // MARK: UISceneSession Lifecycle
