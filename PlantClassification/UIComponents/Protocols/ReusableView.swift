9d7e7790da73a97c04aff4c9d51d38b2c4cc38f6
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-02-21 22:42:46 +0300

    ReusableView Protocol created.

---
+++ PlantClassification/UIComponents/Protocols/ReusableView.swift
@@ -0,0 +1,10 @@
+//
+//  ReusableView.swift
+//  PlantClassification
+//
+//  Created by Şehriban Yıldırım on 21.02.2024.
+//
+
+protocol ReusableView: AnyObject {
+    static var identifier: String { get }
+}
