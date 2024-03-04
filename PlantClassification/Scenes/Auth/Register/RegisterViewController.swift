fb38d63a0d0be3afa44d87995d6edfc19e4dc043
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-04 23:34:37 +0300

    RegisterViewController completed.

--- PlantClassification/Scenes/Auth/Register/RegisterViewController.swift
+++ PlantClassification/Scenes/Auth/Register/RegisterViewController.swift
@@ -6,14 +6,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: BaseViewController<RegisterViewModel> {
 
-    private let uiview: UIView = {
-        let view = RegisterView()
-        view.toLoginButton.addTarget(self, action: #selector(toLogin), for: .touchUpInside)
-        return view
-    }()
+    private let registerView = RegisterView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,14 +25,40 @@ extension RegisterViewController{
     }
     
     private func addView(){
-        view.addSubview(uiview)
-        uiview.edgesToSuperview()
+        registerView.toLoginButton.addTarget(self, action: #selector(toLogin), for: .touchUpInside)
+        registerView.registerButton.addTarget(self, action: #selector(register), for: .touchUpInside)
+        view.addSubview(registerView)
+        registerView.edgesToSuperview()
     }
 }
 //MARK: -Actions
 extension RegisterViewController{
+    
     @objc
     private func toLogin(){
         navigationController?.pushViewController(LoginViewController(viewModel: LoginViewModel()), animated: true)
+    }
+    
+    @objc
+    private func register(){
+        self.authenticationRegister(viewController: self)
+    }
+    
+    func authenticationRegister(viewController: UIViewController){
+        if registerView.userSwitch.isOn{
+            if let email = registerView.emailText.text, let password = registerView.passwordText.text{
+                Auth.auth().createUser(withEmail: email, password: password){ authResult, error in
+                    if let error = error {
+                        AlertManager.showAlert(title: localizedString("Alert.error"), message: localizedString("Alert.errorTitleR") + ": \(error.localizedDescription)", viewController: viewController)
+                    } else {
+                        AlertManager.showAlert(title: localizedString("Alert.success"), message: localizedString("Alert.successTitle"), viewController: viewController)
+                    }
+                }
+            }else{
+                AlertManager.showAlert(title: localizedString("Alert.error"), message: localizedString("Alert.nillText"), viewController: viewController)
+            }
+        }else{
+            AlertManager.showAlert(title: localizedString("Alert.alert"), message: localizedString("Alert.switch"), viewController: viewController)
+        }
     }
 }
