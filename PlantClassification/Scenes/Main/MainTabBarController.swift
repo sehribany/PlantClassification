3e820bea7f63bf15afafdb23e6897fb3a815bdf9
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-05 23:56:17 +0300

    MainTabBarController Created.

---
+++ PlantClassification/Scenes/Main/MainTabBarController.swift
@@ -0,0 +1,53 @@
+//
+//  MainTabBarController.swift
+//  PlantClassification
+//
+//  Created by Şehriban Yıldırım on 5.03.2024.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.tintColor = .appTabBarTitle
+        tabBar.backgroundColor = .appTabBar
+        let reminderViewController       = createReminderViewController()
+        let identificationViewController = createIdentificationViewController()
+        let homeViewController           = createHomeViewController()
+        viewControllers = [homeViewController, identificationViewController, reminderViewController,]
+    }
+    
+    private func configureTabBarIcons(navController: MainNavigationController, icon:UIImage?) {
+        navController.tabBarItem.image = icon
+    }
+    
+    private func configureContents() {
+        tabBar.tintColor = .appGray
+    }
+    
+    private func createReminderViewController()-> UINavigationController {
+        let reminderViewModel      = ReminderViewModel()
+        let reminderViewController = ReminderViewController(viewModel: reminderViewModel)
+        let navController          = MainNavigationController(rootViewController: reminderViewController)
+        configureTabBarIcons(navController: navController, icon: UIImage(named: "icReminder"))
+        return navController
+    }
+    
+    private func createIdentificationViewController()-> UINavigationController {
+        let identificationViewModel      = IdentificationViewModel()
+        let identificationViewController = IdentificationViewController(viewModel: identificationViewModel)
+        let navController                = MainNavigationController(rootViewController: identificationViewController)
+        configureTabBarIcons(navController: navController, icon: UIImage(named: "icScan"))
+        return navController
+    }
+    
+    private func createHomeViewController()-> UINavigationController{
+        let homeViewModel      = HomeViewModel()
+        let homeViewController = HomeViewController(viewModel: homeViewModel)
+        let navController      = MainNavigationController(rootViewController: homeViewController)
+        configureTabBarIcons(navController: navController, icon: UIImage(named: "icHome"))
+        return navController
+    }
+}
