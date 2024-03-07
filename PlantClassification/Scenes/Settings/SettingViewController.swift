474528132e5265246dd0223047b195e6f69c2cbb
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-07 23:14:41 +0300

    SettingViewController Created.

---
+++ PlantClassification/Scenes/Settings/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  PlantClassification
+//
+//  Created by Şehriban Yıldırım on 7.03.2024.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+
+}
