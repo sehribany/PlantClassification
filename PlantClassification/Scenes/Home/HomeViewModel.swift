8a1aaa1d5acf772c48052f9ac9b21e3eba23fd5f
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-03-20 00:26:13 +0300

    HomeViewModel Created.

--- PlantClassification/Scenes/Home/HomeViewModel.swift
+++ PlantClassification/Scenes/Home/HomeViewModel.swift
@@ -19,34 +19,79 @@ protocol HomeViewEventSource {
 protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {}
 
 final class HomeViewModel: BaseViewModel, HomeViewProtocol {
+
     var didSuccessFetchPlants: (() -> Void)?
-    var cellItems: [HomeCellProtocol] = []
-    var page             = 1
+    var cellItems            : [HomeCellModel] = []
+    var cellItemsSearch      : [HomeCellModel] = []
+    var page     = 1
+    var isRequestEnabled = false
     var isPagingEnabled  = false
     
     func numberOfItemsAt(section: Int) -> Int {
-        return cellItems.count
+        if !cellItemsSearch.isEmpty {
+            return cellItemsSearch.count
+        } else {
+            return cellItems.count
+        }
     }
     
     func cellItemAt(indexPath: IndexPath) -> HomeCellProtocol {
-        return cellItems[indexPath.row]
+        if !cellItemsSearch.isEmpty {
+            return cellItemsSearch[indexPath.row]
+        } else {
+            return cellItems[indexPath.row]
+        }
     }
-    
 }
-//MARK: - Network
-extension HomeViewModel{
-    func fetchPlants(completion: @escaping (Error?) -> Void) {
-        PlantManager.shared.fetchPlants { [weak self] result in
+// MARK: - Network
+extension HomeViewModel {
+    
+    func fetchPlants(page: Int) {
+        let request = GetPlantsRequest(page: page)
+        self.isRequestEnabled = false
+        if page == 1 { showActivityIndicatorView?() }
+        dataProvider.request(for: request) { [weak self] result in
             guard let self = self else { return }
-            switch result {
+            self.hideActivityIndicatorView?()
+            self.isRequestEnabled = true
+            switch result{
             case .success(let response):
-                let cellItem = response.map({ HomeCellModel(plant: $0) })
-                self.cellItems.append(contentsOf: cellItem)
-                self.page += 1
+                self.cellItems.removeAll()
+                let cellItems = response.data.map({HomeCellModel(plant: $0 )})
+                self.cellItems.append(contentsOf: cellItems)
                 self.didSuccessFetchPlants?()
             case .failure(let error):
-                print("Hata: \(error)")
+                print("\(error.localizedDescription)")
             }
         }
+    }
+    
+    func fetchMorePages(){
+        let nextPage = self.page + 1
+        fetchPlants(page: nextPage)
+        self.page = nextPage
+    }
+    
+    func fetchSearch(searchTitle: String){
+        let request = GetSearchPlantsRequest(searchTitle: searchTitle)
+        self.isRequestEnabled = false
+        dataProvider.request(for: request) { [weak self] result in
+            guard let self = self else { return }
+            self.hideActivityIndicatorView?()
+            self.isRequestEnabled = true
+            switch result{
+            case .success(let response):
+                self.cellItemsSearch.removeAll()
+                let cellItems = response.data.map({HomeCellModel(plant: $0 )})
+                self.cellItemsSearch.append(contentsOf: cellItems)
+                self.didSuccessFetchPlants?()
+            case .failure(let error):
+                print("\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func fetchMorePages(searchTitle: String) {
+        fetchSearch(searchTitle: searchTitle)
     }
 }
